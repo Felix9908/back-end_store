6af90bb3440e88121eb7b05dcb7b7aebda894b45
Felix9908 <frfyfelix@gmail.com>
2023-11-26 13:27:01 -0500

    configuracion de la base de datos

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
 export const PORT = process.env.PORT || 9999;
 
-// export const  DB_HOST = process.env.DB_HOST || "monorail.proxy.rlwy.net"
-// export const  DB_USER = process.env.DB_USER || "root"
-// export const  DB_PASSWORD = process.env.DB_PASSWORD || "G5bcEbGaBgf6FaHe3GaHgBEfC6adeaAA"
-// export const  DB_DATABASE = process.env.DB_DATABASE || "railway"
-// export const  DB_PORT = process.env.DB_PORT || "40389"
-
-export const  DB_HOST = process.env.DB_HOST || "localhost"
+export const  DB_HOST = process.env.DB_HOST || "monorail.proxy.rlwy.net"
 export const  DB_USER = process.env.DB_USER || "root"
-export const  DB_PASSWORD = process.env.DB_PASSWORD || ""
-export const  DB_DATABASE = process.env.DB_DATABASE || "store"
-export const  DB_PORT = process.env.DB_PORT || "3306"
+export const  DB_PASSWORD = process.env.DB_PASSWORD || "G5bcEbGaBgf6FaHe3GaHgBEfC6adeaAA"
+export const  DB_DATABASE = process.env.DB_DATABASE || "railway"
+export const  DB_PORT = process.env.DB_PORT || "40389"
